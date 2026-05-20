3ce56c56c8bd12ff2fc584ea9d2c8d582a0b6b5e
Anton Artemenko <muroshechka@gmail.com>
2026-05-20 10:30:00 +0700

    puf_core: add IOscillator, IPufGenerator interfaces and Fingerprint type

---
+++ components/puf_core/i_puf_generator.hpp
@@ -0,0 +1,28 @@
+/// @file i_puf_generator.hpp
+/// @author Artemenko Anton
+/// @brief Интерфейс генератора аппаратного идентификатора
+
+#ifndef GUID_84347C5B_5509_423B_8CAA_92CF9514EFD4
+#define GUID_84347C5B_5509_423B_8CAA_92CF9514EFD4
+
+#include <fingerprint.hpp>
+
+#include <cstddef>
+
+namespace puf {
+
+/// @brief Генератор отпечатка устройства
+class IPufGenerator {
+public:
+    virtual ~IPufGenerator() = default;
+
+    /// @brief Генерирует отпечаток устройства
+    virtual Fingerprint Generate() = 0;
+
+    /// @return Длина отпечатка в битах
+    virtual size_t FingerprintBits() const = 0;
+};
+
+} // namespace puf
+
+#endif // GUID_84347C5B_5509_423B_8CAA_92CF9514EFD4
